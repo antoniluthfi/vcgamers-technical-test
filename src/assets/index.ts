093b489ada3ead02f9fcbc9e6bb0014a94c49553
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-08-02 18:04:49 +0800

    set image assets

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+export const Thumbnail1 = require('./thumbnail_1.png');
+export const Thumbnail2 = require('./thumbnail_2.png');
+export const Thumbnail3 = require('./thumbnail_3.png');
+export const Thumbnail4 = require('./thumbnail_4.png');
+export const Thumbnail5 = require('./thumbnail_5.png');
+export const Thumbnail6 = require('./thumbnail_6.png');
+export const Thumbnail7 = require('./thumbnail_7.png');
+export const Thumbnail8 = require('./thumbnail_8.png');
+export const ImageNotAvailable = require('./image_not_available.png');
